2064ad7515f8a9dff71a10493887b3f76d7ec05b
qiao <840569784@qq.com>
2017-04-12 16:00:58 +0800

    you can ignore

--- blog/static/js/displayCtrl.js
+++ blog/static/js/displayCtrl.js
@@ -4,9 +4,11 @@ var device_width=window.outerWidth;//outerWidth和outerHeight属性,可以获取
 // 内部宽高是指除去菜单栏、工具栏、边框等占位元素后，用于显示网页的净宽高
 var strong_ctrl1=document.getElementById('ctrl1');//设定在小屏幕(<700px)和大屏幕Home，Blog的大小
 var strong_ctrl2=document.getElementById('ctrl2');
+var jumbotron=document.getElementById('jumbotron')
 if(device_width<700){//对Home，Blog大小控制
 	strong_ctrl1.style.fontSize='13px';
 	strong_ctrl2.style.fontSize='13px'
+	jumbotron.style.height='160px'
 }
 else{
 	strong_ctrl1.style.fontSize='26px';
